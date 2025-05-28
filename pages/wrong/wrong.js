c7c4f1de308f32bebb59081951248cd06fcfa2d5
安然 梁 <211250127@smail.nju.edu.cn>
2025-05-28 19:54:13 +0800

    fix: error question

--- pages/wrong/wrong.js
+++ pages/wrong/wrong.js
@@ -4,7 +4,9 @@ const app = getApp();
 const db = wx.cloud.database();
 // 获取集合的引用
 const activityQuestion = db.collection('activityQuestion');
-const WrongQuestion  = db.collection('WrongQuestion');
+const errorqs  = db.collection('WrongQuestion');
+const activityUser = db.collection('activityUser');
+
 // 数据库操作符
 const _ = db.command;
 
@@ -16,14 +18,22 @@ Page({
   data: {
     questionList: [],
     index: 0,
+    positionId:0,
+    typeId:null,
 
     chooseValue: [],
-    showAnswer: true,
+    showAnswer: [],
+    colorList:[],
 
     totalScore: 0,
     wrong: 0,
-    wrongListId: {},
-
+    wrongListId: [],
+    wrongListSort: [],
+    questionNumber:{
+      right:0,
+      error:0,
+      count:0
+    }
   },
 
   /**
@@ -34,12 +44,20 @@ Page({
     this.getQuestionList(app.globalData.openid);
   },
 
+  onUnload: function () {
+    wx.setStorageSync('chooseValue', this.data.chooseValue)
+    wx.setStorageSync('showAnswer', this.data.showAnswer)
+    wx.setStorageSync('wrong', this.data.wrong)
+    wx.setStorageSync('colorList', this.data.colorList)
+    return
+  },
+
   // 获取错题库-函数定义
   getQuestionList(openid) {
     wx.showLoading({
       title: '拼命加载中'
     });
-    WrongQuestion
+    errorqs
     .where({       
       _openid:openid,
     }).get()
@@ -71,6 +89,11 @@ Page({
   radioChange(e){
     this.data.chooseValue[this.data.index] = e.detail.value;
   },
+
+  confirmAnswer(){
+    this.chooseJudge();
+},
+
 
   // 下一题/提交 按钮
   nextSubmit(){
@@ -108,7 +131,166 @@ Page({
     }
   },
 
+  // 判断所选择的选项是否为正确答案
+  chooseJudge(){
+    if(this.data.showAnswer[this.data.index] == true){
+      return wx.showToast({
+        title: '已提交答案!',
+        icon: 'none'
+      })
+    }
 
+    if (this.data.chooseValue[this.data.index] == undefined || this.data.chooseValue[this.data.index].length == 0)
+    {  
+      return wx.showToast({
+        title: '请选择答案!',
+        icon: 'none'
+      })
+    }
+
+    var trueValue = this.data.questionList[this.data.index]['true'];
+    var chooseVal = this.data.chooseValue[this.data.index];
+    console.log("chooseVal: ", chooseVal)
+    console.log("trueValue: ", trueValue)
+
+    var wrongid = this.data.questionList[this.data.index]._id;
+    
+    // 判断是否为多选题
+    var isMultiQuestion = trueValue.length > 1;
+    var isCorrect = false;
+    
+    if (Array.isArray(chooseVal)) {
+      // 将用户选择的选项转换为字符串数组
+      var chooseValStr = chooseVal.join('');
+    }
+    if (isMultiQuestion) {
+      // 多选题判断逻辑
+      // 检查用户选择的选项数量是否与正确答案相同
+      if (chooseVal.length === trueValue.length) {
+        // 对用户选择的选项进行排序，然后与正确答案比较
+        var sortedChooseVal = chooseValStr.split('').sort().join('');
+        var sortedTrueValue = trueValue.split('').sort().join('');
+        isCorrect = (sortedChooseVal === sortedTrueValue);
+      }
+    } else {
+      // 单选题判断逻辑
+      isCorrect = (chooseVal.toString() === trueValue.toString());
+    }
+    
+    if (!isCorrect) {
+      wx.showToast({
+        title: '答案错误! ',
+        icon: 'none'
+      })
+      // 答错
+      console.log('false');
+      this.data.wrongListSort.push(this.data.index);
+      this.data.wrongListId.push(this.data.questionList[this.data.index]._id);
+      this.data.questionNumber.error++;
+    } else {
+      wx.showToast({
+        title: '答案正确!',
+        icon: 'none'
+      })
+      // 答对则累计总分
+      this.ErrorQSUpdate(wrongid);
+      // 更新答题数  
+      this.QuestionNumUpdate();
+      this.setData({
+        totalScore: this.data.totalScore + 5,
+      })
+      this.data.questionNumber.right++;
+    }
+    // 修改css
+    let rightAnswer = this.data.questionList[this.data.index].true
+    let chooseValue = this.data.chooseValue[this.data.index]
+    this.data.colorList[this.data.index] = {}
+    
+    // 先将所有用户选择的选项设置为红色
+    for(let i=0;i<chooseValue.length;i++){
+      this.data.colorList[this.data.index][chooseValue[i]]= 'rgba(255, 0, 0, 0.473)'
+    }
+    
+    // 再将所有正确答案设置为绿色
+    for(let i=0;i<rightAnswer.length;i++){
+      this.data.colorList[this.data.index][rightAnswer[i]]= 'rgba(0, 128, 0, 0.452)'
+      if(!isCorrect && chooseValue.indexOf(rightAnswer[i]) !== -1){
+        this.data.colorList[this.data.index][rightAnswer[i]]= 'rgba(255, 255, 0, 0.5)' // 黄色
+      }
+    }
+
+    //题目记录
+    this.data.questionNumber.count++;
+    this.data.showAnswer[this.data.index] = true;
+    this.setData({
+      questionNumber: this.data.questionNumber,
+      showAnswer: this.data.showAnswer,
+      colorList : this.data.colorList
+    })
+    console.log(this.data.colorList)
+  },
+
+  //更新错题
+  ErrorQSUpdate(wrongId){
+    errorqs.where({
+      questionId:wrongId,
+    }).get()
+    .then(res=>{
+      if(res.data == undefined || res.data == null){
+        console.log('never wrong!');
+      }
+      else{
+        errorqs.doc(res.data[0]._id).remove()
+        .then(res=>{
+          console.log('delete wrong question');
+        })
+        .catch(err=>{
+          console.log('delete error');
+        })
+      }
+    })
+    .catch(err=>{
+      console.log('error question update error');
+    })
+  },
+
+   //更新已答题数目
+   QuestionNumUpdate(){
+    let openId = app.globalData.openid
+    activityUser.where({
+      _openid: openId
+    })
+    .update({
+      data:{
+        question_num:_.inc(1)
+      },
+    })
+    .then(res=>{
+      if(res.stats.updated===0){
+        this.addQuestionNum()
+        console.log("addQuestionNum!")
+      }
+    })
+    .catch(err=>{
+      console.log(err);
+      console.log('QuestionNumUpdate error!');
+    })
+  
+  },
+  // 新增记录项
+  addQuestionNum(){
+    let userInfo = app.globalData.userInfo
+    activityUser.add({
+      data:{
+        username:userInfo.nickName,
+        avatarUrl:userInfo.avatarUrl,
+        question_num:1
+      },
+    }).then(res=>{
+       console.log(res);
+    })
+
+  },
   // 查看答卷
   seeExamRecord(){
     
